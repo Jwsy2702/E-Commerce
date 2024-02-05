3bc550d5241a6e120a98c3044bf7f7cd124a5b41
Jwsy2702 <jonathanwsy@hotmail.com>
2024-02-05 16:40:26 +0800

    Breadcrum links added

--- frontend/src/Components/Breadcrums/Breadcrum.jsx
+++ frontend/src/Components/Breadcrums/Breadcrum.jsx
@@ -1,13 +1,32 @@
 import React from "react";
 import "./Breadcrum.css";
 import arrow_icon from "../Assets/breadcrum_arrow.png";
+import { Link } from "react-router-dom";
 
 const Breadcrum = (props) => {
   const { product } = props;
+  console.log("product", product);
+  function convertToSlug(Text) {
+    return Text.replace(/([a-z])([A-Z])/g, "$1-$2") // Add hyphen between lowercase and uppercase letters
+      .replace(/[\s_]+/g, "-") // Replace spaces with hyphens
+      .toLowerCase(); // Convert the entire string to lowercase
+  }
+  function convertToTitleCase(Text) {
+    return Text.replace(/([a-z])([A-Z])/g, "$1 $2").replace(/\b\w/g, (char) =>
+      char.toUpperCase()
+    );
+  }
+
+  const slug = convertToSlug(product.category);
+  const title = convertToTitleCase(product.category);
+
   return (
     <div className="breadcrum">
-      HOME <img src={arrow_icon} alt="" /> SHOP <img src={arrow_icon} alt="" />{" "}
-      {product.category} <img src={arrow_icon} alt="" /> {product.name}
+      {/*Could use Link or onClick, Link for more straightforward and consistent navigation. Onclick for custom logic, program navigation to conditions, states, etc */}
+      <Link to="/">HOME</Link> <img src={arrow_icon} alt="" />
+      <Link to="/shop">SHOP</Link> <img src={arrow_icon} alt="" />
+      <Link to={`/${slug}`}>{title}</Link>
+      <img src={arrow_icon} alt="" /> {product.name}
     </div>
   );
 };
